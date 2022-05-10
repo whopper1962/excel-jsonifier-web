ca8c80d8c87739bd4e2ec56afa05b8a68c8fcedb
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-05-11 07:02:49 +0900

    created file upload component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="main-div">
+<div class="main-div hoge">
   <h2>Excel JSONifier</h2>
   <p>created by Masashi Kawakami</p>
   <div class="form-width">
@@ -9,37 +9,58 @@
       drop-placeholder="Drop file here..."
       accept=".xlsx"
     ></b-form-file>
+    <Select
+      :values="columnOptions"
+      :keyForOptions="`num`"
+      :value="selectedColumn"
+      :disabled="!isExcelUploaded"
+      @input="inputValue"
+    />
+    <Select
+      :values="columnOptionsTwo"
+      :keyForOptions="`num`"
+      :value="selectedColumnTwo"
+      :disabled="!isExcelUploaded"
+      @input="inputValueTwo"
+    />
   </div>
-  <button class="btn btn-success" @click="jsonify()" :disabled="!file1">
-    JSONIFY!
+  <button
+    :class="isExcelUploaded ? `btn btn-success` : `btn btn-primary`"
+    @click="isExcelUploaded ? jsonify() : readFile()" :disabled="!file1"
+  >
+    {{ isExcelUploaded ? `JSONIFY!` : `READ!` }}
   </button>
 </div>
 </template>
 <script>
+import Select from '@/components/Select';
 export default {
   components: {
+    Select
   },
   data () {
     return {
-      file1: null
+      file1: null,
+      columnOptions: [],
+      columnOptionsTwo: [],
+      selectedColumn: '',
+      selectedColumnTwo: '',
+      isExcelUploaded: false
     };
   },
   methods: {
     jsonify () {
       this.$toasted.show(`JSONFIED!`);
+    },
+    readFile () {
+      this.isExcelUploaded = true;
+    },
+    inputValue (newVal) {
+      this.selectedColumn = newVal;
+    },
+    inputValueTwo (newVal) {
+      this.selectedColumnTwo = newVal;
     }
   }
 }
 </script>
-<style>
-.main-div {
-  padding-top: 20vh;
-}
-.form-width {
-  width: 500px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-bottom: 3vh;
-  padding-top: 2vh;
-}
-</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="select-form">
+  <select class="form-control" v-model="selectValue" :disabled="disabled">
+    <template v-for="(val, index) in values">
+      <option :key="`value_${index}`">{{ val[keyForOptions] }}</option>
+    </template>
+  </select>
+</div>
+</template>
+<script>
+
+export default {
+  props: {
+    values: {type: Array},
+    value: {type: String},
+    keyForOptions: {type: String},
+    disabled: {type: Boolean, default: false}
+  },
+  computed: {
+    selectValue: {
+      get () {
+        return this.value;
+      },
+      set (value) {
+        this.$emit('input', value);
+      }
+    }
+  }
+}
+</script>
